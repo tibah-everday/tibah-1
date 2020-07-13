40d947e1f24e0535274aab38e1b300119544e190
thd2611 <45746141+thd2611@users.noreply.github.com>
2020-07-13 17:19:33 +0900

    JS: Navbar menu scroll

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ document.addEventListener("scroll", () => {
     navBar.classList.remove("navbar--dark");
   }
 });
+
+// 네비바 메뉴 클릭시 해당 섹션으로 스크롤 이동
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  //   console.log(link);
+  if (link === undefined) {
+    return;
+  } else {
+    console.log(link);
+  }
+  //   link.scrollIntoView();
+  console.log(link);
+  const scrollTo = document.querySelector(link);
+  console.log(scrollTo);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+  //   link.scrollIntoView();
+});
